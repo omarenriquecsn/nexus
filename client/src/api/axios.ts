65965cfc569e9b1150bbb4e833a4e9952603afaf
omarenriquecsn <omarenriquecs@gmail.com>
2026-04-05 18:12:49 -0400

    api

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api', // Tu backend operativo
+  baseURL: '/api', // Tu backend operativo
 });
 
 export default api;
