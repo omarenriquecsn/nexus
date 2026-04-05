42547e4f61d014df3aaae25815f7b117efc5134b
omarenriquecsn <omarenriquecs@gmail.com>
2026-04-05 14:33:53 -0400

    nexus

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,7 +1,7 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from '@prisma/client';
 
-const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://postgres:TU_PASSWORD@localhost:5432/nexus_core?schema=public';
+const DATABASE_URL = process.env.DATABASE_URL || "postgresql://postgres:nexus_pass_2026@db:5432/nexus_db?schema=public";
 
 const adapter = new PrismaPg({ 
     connectionString: DATABASE_URL});
